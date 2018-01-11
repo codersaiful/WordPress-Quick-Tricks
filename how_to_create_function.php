2f96a3406c8b72086a30f608917a93e41a82cb44
Saiful Islam <codersaiful@gmail.com>
2018-01-11 20:39:33 +0600

    Function created

--- how_to_create_function.php
+++ how_to_create_function.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Function Description here. Suppose: Customizing post title for our [Product Name]
+ * 
+ * @since version
+ * @param String $sample_param Its a sample Param
+ * @return String NOramlly it will return a Customize Post's Title
+ * 
+ * @since 1.0
+ * @link http://coder_url_for_related_function.com Description for this link.
+ */
+function sip_post_title_customize($sample_param){
+    //Do something, What you want to do
+    return $sample_param;
+}
